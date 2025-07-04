48adb1c54526e31500901d37c28eab9d0945acbb
Lukaku092 <lukakuberdarah22@gmail.com>
2025-07-04 18:57:48 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "luka098"; // Ganti dengan domain utama kalian
-const serviceName = "fibele5854.workers.dev"; // Ganti dengan nama workers kalian
+const rootDomain = "fibele5854.workers.dev"; // Ganti dengan domain utama kalian
+const serviceName = "luka098"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
